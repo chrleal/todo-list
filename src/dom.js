d87e93600c91b045ef4764dc5b69649e27d253b3
chrleal <chr.leal@gmail.com>
2022-07-22 18:45:48 -0300

    Add functionality to create and display projects

--- src/dom.js
+++ src/dom.js
@@ -2,10 +2,12 @@ import * as todo from "./todo";
 
 let indexTask;
 const modalOverlay = document.querySelector('.modal-overlay');
-const modal = document.querySelector('.modal');
+const modal = document.querySelector('.modal-todo');
 const modalTitle = document.querySelector('.modal-title');
-const form = document.querySelector('#form');
+const form = document.querySelector('#form-todo');
+const formProject = document.querySelector('#form-project');
 const title = document.querySelector('#title');
+const titleProject = document.querySelector('#title-project')
 const description = document.querySelector('#description');
 const date = document.querySelector('#date');
 const priority = document.querySelector('#priority');
@@ -13,6 +15,29 @@ const closeButton = document.querySelector('.close');
 const saveEditBtn = document.querySelector('#save-edit-button');
 const addButton = document.querySelector('.add-button');
 const todoContainer = document.querySelector('.todo-container');
+const projectBtn = document.querySelector('#project-button');
+const projectContainer = document.querySelector('.sidebar-project');
+const heading = document.querySelector('.heading');
+const sidebarBtns = document.querySelectorAll('.sidebar-item-content');
+
+(function sidebarEventHandler(){
+  sidebarBtns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      heading.textContent = btn.querySelector('p').textContent;
+      removeAllTasks();
+      showTask();
+      addButton.style.display = 'flex';
+      if (e.target.id != 'inbox'){
+        addButton.style.display = 'none';
+        removeAllTasks();
+        showTask();
+        // if (e.target.id == today){
+          
+        // }
+      }
+    })
+  })
+})();
 
 (function toggleModal(){
     showModal();
@@ -27,8 +52,24 @@ function showModal(){
     modalOverlay.style.display = 'block';
     modalTitle.textContent = "New Task";
     saveEditBtn.textContent = "Save";
+    saveEditBtn.classList.remove('save-project');
     saveEditBtn.classList.remove('edit');
     saveEditBtn.classList.add('save');
+    form.style.display = 'block';
+    formProject.style.display = 'none';
+  })
+
+   projectBtn.addEventListener('click', () => {
+    modal.style.display = 'flex';
+    modalOverlay.style.display = 'block';
+    modalTitle.textContent = "New Project";
+    saveEditBtn.textContent = "Save";
+    saveEditBtn.classList.remove('edit');
+    saveEditBtn.classList.remove('save');
+    saveEditBtn.classList.add('save-project');
+    form.style.display = 'none';
+    formProject.style.display = 'block';
+    formProject.reset();
   })
 }
 
@@ -44,64 +85,87 @@ function resetDisplay() {
 }
 
 (function submitForm(){
-  saveEditBtn.addEventListener('click', (e) => {
-    if (e.target.classList.contains('save')){
+  saveEditBtn.addEventListener('click', () => {
+    if (saveEditBtn.classList.contains('save')){
       todo.submitTask();
-      resetDisplay();
-      removeAll();
+      removeAllTasks();
       showTask();
-    } else if (e.target.classList.contains('edit')){
+      console.log(todo.myTasks);
+    } else if (saveEditBtn.classList.contains('edit')){
       todo.editTask(indexTask);
-      resetDisplay();
-      removeAll();
+      removeAllTasks();
       showTask();
-      console.log("dentro do botao" + indexTask)
+      console.log(todo.myTasks);
+    } else if (saveEditBtn.classList.contains('save-project')){
+      todo.submitProject();
+      removeAllProjects()
+      showProject();
+      console.log(todo.myProjects)
     }
   })
 })();
 
+function showProject(){
+  todo.myProjects.forEach((project) => {
+    const newProject = document.createElement('div');
+    newProject.classList.add('sidebar-project-content');
+    newProject.setAttribute('id', 'project');
+    newProject.textContent = project.title;
+    projectContainer.appendChild(newProject);
+    newProject.addEventListener('click', () => {
+      addButton.style.display = 'flex';
+      removeAllTasks();
+      heading.textContent = project.title;
+      showTask();
+    })
+  })
+}
+
 function showTask(){
   todo.myTasks.forEach((task, index) => {
-    const taskContainer = document.createElement('div');
-    taskContainer.setAttribute('class', 'task-container')
-    taskContainer.setAttribute('data-id', `${index}`)
-    todoContainer.insertBefore(taskContainer, addButton);
+    if (task.project == heading.textContent){
+      const taskContainer = document.createElement('div');
+      taskContainer.setAttribute('class', 'task-container')
+      taskContainer.setAttribute('data-id', `${index}`)
+      todoContainer.insertBefore(taskContainer, addButton);
 
-    const taskCheck = document.createElement('input');
-    taskContainer.appendChild(taskCheck);
-    taskCheck.setAttribute('type', 'checkbox');
+      const taskCheck = document.createElement('input');
+      taskContainer.appendChild(taskCheck);
+      taskCheck.setAttribute('type', 'checkbox');
 
-    const taskTitle = document.createElement('h2');
-    taskContainer.appendChild(taskTitle);
-    taskTitle.textContent = task.title;
+      const taskTitle = document.createElement('h2');
+      taskTitle.classList.add('task-title');
+      taskContainer.appendChild(taskTitle);
+      taskTitle.textContent = task.title;
 
-    const taskEditBtn = document.createElement('span');
-    taskContainer.appendChild(taskEditBtn);
-    taskEditBtn.setAttribute('class', 'material-icons-outlined edit-button');
-    taskEditBtn.textContent = "edit_note";
-    
-    taskEditBtn.addEventListener('click', (e) => {
-      indexTask = e.target.parentElement.getAttribute('data-id');
-      console.log("dentro do lapis" + indexTask + index);
-      modal.style.display = 'flex';
-      modalOverlay.style.display = 'block';
-      modalTitle.textContent = "Edit Task";
-      saveEditBtn.textContent = "Edit";
-      saveEditBtn.classList.remove('save');
-      saveEditBtn.classList.add('edit');
-      editModal(task.title, task.description, task.date, task.priority);
-    })
+      const taskEditBtn = document.createElement('span');
+      taskContainer.appendChild(taskEditBtn);
+      taskEditBtn.setAttribute('class', 'material-icons-outlined edit-button');
+      taskEditBtn.textContent = "edit_note";
+      
+      taskEditBtn.addEventListener('click', (e) => {
+        indexTask = e.target.parentElement.getAttribute('data-id');
+        modal.style.display = 'flex';
+        modalOverlay.style.display = 'block';
+        modalTitle.textContent = "Edit Task";
+        saveEditBtn.textContent = "Edit";
+        saveEditBtn.classList.remove('save-project');
+        saveEditBtn.classList.remove('save');
+        saveEditBtn.classList.add('edit');
+        editModal(task.title, task.description, task.date, task.priority);
+      })
 
-    const taskDeleteBtn = document.createElement('span');
-    taskContainer.appendChild(taskDeleteBtn);
-    taskDeleteBtn.setAttribute('class', 'material-icons-outlined delete-button');
-    taskDeleteBtn.textContent = "delete_outline";
+      const taskDeleteBtn = document.createElement('span');
+      taskContainer.appendChild(taskDeleteBtn);
+      taskDeleteBtn.setAttribute('class', 'material-icons-outlined delete-button');
+      taskDeleteBtn.textContent = "delete_outline";
 
-    taskDeleteBtn.addEventListener('click', () => {
-      console.log(task);
-      todoContainer.removeChild(taskContainer);
-      todo.deleteTask(task);
-    })
+      taskDeleteBtn.addEventListener('click', () => {
+        console.log(task);
+        todoContainer.removeChild(taskContainer);
+        todo.deleteTask(task);
+      })
+    }
   })
 }
 
@@ -112,9 +176,14 @@ function editModal(titleEdit, descriptionEdit, dateEdit, priorityEdit) {
   priority.value = priorityEdit;
 }
 
-function removeAll(){
+function removeAllTasks(){
   const removeDiv = todoContainer.querySelectorAll('.task-container');
   removeDiv.forEach(div => div.remove());
 }
 
-export {title, description, date, priority, showTask}
+function removeAllProjects(){
+  const removeProject = projectContainer.querySelectorAll('#project');
+  removeProject.forEach(div => div.remove());
+}
+
+export {heading, title, titleProject, description, date, priority, showTask, resetDisplay}
--- src/todo.js
+++ src/todo.js
@@ -2,26 +2,46 @@ import * as dom from "./dom";
 
 let validate;
 const myTasks = [];
+const myProjects = [];
 
-const taskFactory = (title, description, date, priority) => {
-  return {title, description, date, priority}
+const taskFactory = (title, description, date, priority, project) => {
+  return {title, description, date, priority, project}
+}
+
+const projectFactory = (title) => {
+  return {title}
 }
 
 function validateInput() {
-  if (dom.title.value != ""){
+  if (dom.title.value != "" || dom.titleProject.value != ""){
     return validate = true;
   } else {
-    dom.title.reportValidity();
     return validate = false;
   }
 }
 
 function submitTask(){
   if (validateInput() == true) {
-    const newTask = taskFactory(dom.title.value, dom.description.value, dom.date.value, dom.priority.value);
+    const newTask = taskFactory(dom.title.value, dom.description.value, dom.date.value, dom.priority.value, dom.heading.textContent);
     myTasks.push(newTask);
-    console.log(myTasks);
+    dom.resetDisplay();
+  } else {
+    dom.title.reportValidity();
   }
+}
+
+function submitProject() {
+  if (validateInput() == true) {
+    const newProject = projectFactory(dom.titleProject.value);
+    myProjects.push(newProject);
+    dom.resetDisplay();
+  } else {
+    dom.title.reportValidity();
+  }
+}
+
+function selectProject(){
+  
 }
 
 function deleteTask(task){
@@ -34,6 +54,7 @@ function editTask(index) {
     myTasks[index].description = dom.description.value;
     myTasks[index].date = dom.date.value;
     myTasks[index].priority = dom.priority.value;
+    dom.resetDisplay();
   }
 }
 
@@ -41,4 +62,4 @@ const task = taskFactory('test', 'test', '19/07/22', 'high');
 myTasks.push(task);
 console.log(myTasks);
 
-export {myTasks, submitTask, deleteTask, editTask}
+export {myTasks, myProjects, submitTask, deleteTask, editTask, submitProject}
